2a17e1b7dbd9e4b3f0a5ad22fed689fed8d54e72
CableSynth <lorencalebbaum@gmail.com>
2021-03-13 07:32:33 -0500

    Added loop with quit

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
-use std::io;
+use std::io::{Read, Write, stdout, stdin, Error};
 use tui::Terminal;
 use tui::backend::TermionBackend;
 use termion::raw::IntoRawMode;
+use termion::input;
+use termion::screen::AlternateScreen;
+use termion::event::Key;
 use tui::widgets::{Widget, Block, Borders};
 use tui::layout::{Layout, Constraint, Direction};
 
-fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout().into_raw_mode()?;
+fn main() -> Result<(), Error> {
+    let stdout = stdout().into_raw_mode()?;
+    let stdin = stdin();
+    let mut stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
-    terminal.draw(|f| {
+    terminal.draw(|frame| {
         let chunks = Layout::default()
             .direction(Direction::Vertical)
             .margin(1)
             .constraints(
                 [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10)
+                    Constraint::Percentage(7),
+                    Constraint::Percentage(85),
+                    Constraint::Percentage(8)
                 ].as_ref()
             )
-            .split(f.size());
-        let block = Block::default()
-             .title("Block")
+            .split(frame.size());
+        let search = Block::default()
+             .title("Search")
              .borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
-        let block = Block::default()
+        frame.render_widget(search, chunks[0]);
+        let songs = Block::default()
              .title("Block 2")
              .borders(Borders::ALL);
-        f.render_widget(block, chunks[1]);
-    })
+        frame.render_widget(songs, chunks[1]);
+
+    })?;
+    let mut bytes = stdin.bytes();
+    loop {
+        let b = bytes.next().unwrap();
+
+        match b.unwrap() {
+            b'q' => break Ok(()),
+            a => (),
+        };
+        // backend.flush();
+    }
 }
